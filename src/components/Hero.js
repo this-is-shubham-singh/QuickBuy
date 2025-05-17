b723a5d59eac5abee9fc49099147b2cf7e3f0a37
shubham.sc <shubham@skills.cafe>
2025-05-17 21:41:00 +0530

    hero section completed

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,22 @@
-import React from 'react'
+// HeroSection.jsx
+import React from "react";
+import { assets } from "../assets/frontend_assets/assets";
 
-const Hero = () => {
+const HeroSection = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <div className="hero-section">
+      <div className="hero-content">
+        <div className="hero-text">
+          <h3 className="hero-subtitle">----  OUR BESTSELLERS</h3>
+          <h1 className="hero-title">Latest Arrivals</h1>
+          <h3 className="hero-subtitle">SHOP NOW  -----</h3>
+        </div>
+        <div className="hero-image-wrapper">
+          <img className="hero-image" src={assets.hero_img} alt="Bestseller" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Hero
+export default HeroSection;
---
+++ src/context/shopContext.js
@@ -0,0 +1,9 @@
+import React, { createContext } from "react";
+
+const dataContext = createContext();
+
+const shopContext = () => {
+  return <div>shopContext</div>;
+};
+
+export default shopContext;
