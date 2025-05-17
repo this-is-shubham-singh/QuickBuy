e19d21fa80a5f2cc25cf04912073f233867b41ed
shubham.sc <shubham@skills.cafe>
2025-05-17 17:24:49 +0530

    navbar completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import Collection from "./pages/Collection";
+import Contact from "./pages/Contact";
+import Orders from "./pages/Orders";
+import PlaceOrder from "./pages/PlaceOrder";
+import Product from "./pages/Product";
+import Navbar from "./components/Navbar";
+import About from "./pages/About";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/collection" element={<Collection />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/placeOrder" element={<PlaceOrder />} />
+        <Route path="/product" element={<Product />} />
+      </Routes>
+    </div>
+  );
+};
 
-export default App
+export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,50 @@
-import React from 'react'
+// Navbar.jsx
+import React from "react";
+import { FaSearch, FaUser, FaShoppingCart } from "react-icons/fa";
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="navbar">
+      <Link to={"/"} className="navbar-logo">
+        QuickBuy
+      </Link>
+      <ul className="navbar-links">
+        <li className="navbar-link-item">
+          <NavLink className="navbar-link" to={"/"}>
+            Home
+          </NavLink>
+        </li>
+        <li className="navbar-link-item">
+          <NavLink className="navbar-link" to={"/collection"}>
+            Collection
+          </NavLink>
+        </li>
+        <li className="navbar-link-item">
+          <NavLink className="navbar-link" to={"/about"}>
+            About
+          </NavLink>
+        </li>
+        <li className="navbar-link-item">
+          <NavLink className="navbar-link" to={"/contact"}>
+            Contact
+          </NavLink>
+        </li>
+      </ul>
+      <div className="navbar-icons">
+        <Link to={"./collection"}>
+          <FaSearch className="navbar-icon" />
+        </Link>
+        <FaUser className="navbar-icon" />
+        <div className="navbar-cart-wrapper">
+          <Link to={"/cart"}>
+            <FaShoppingCart className="navbar-icon" />
+            <span className="navbar-cart-count">2</span>
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
